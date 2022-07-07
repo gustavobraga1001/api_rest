088053429b563b393f1952fa22d45fd7d8f10a27
gustavobraga1001 <gubraro0@gmail.com>
2022-07-07 17:04:44 -0300

    primeiro commit

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Event::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        {
+            Event::create($request->all());
+            return $response = json_encode([
+                "error" => false,
+                "mensage" => "Data publicada com successo"
+            ]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Event::findOrFail($id, ['id','date']);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $event = Event::findOrFail($id);
+        $event->update($request->all());
+        return $response = json_encode([
+            "error" => false,
+            "mensage" => "Data atualizada com sucesso!"
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $event = Event::findOrFail($id);
+        $event->delete();
+        return $response = json_encode([
+            "error" => false,
+            "mensage" => "Data deletada com sucesso!"
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\PostsController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use app\Models\User;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::apiResource('posts', \App\Http\Controllers\PostsController::class);
+    Route::apiResource('dates', \App\Http\Controllers\EventController::class);
+    route::post('/logout', [AuthController::class, 'logout']);
+});
