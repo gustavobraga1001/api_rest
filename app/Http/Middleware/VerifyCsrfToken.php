085f9906bf5ef8630102d5ddc0c331abbe4b905b
gustavobraga1001 <gubraro0@gmail.com>
2022-07-14 12:21:43 -0300

    teste cors

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'stripe/*',
-        'https://api-braga.herokuapp.com/api/posts',
+        'api/posts',
     ];
 }
