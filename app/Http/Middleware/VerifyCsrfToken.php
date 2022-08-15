414737b548551fa416a977d65b85be11f512f311
gustavobraga1001 <gubraro0@gmail.com>
2022-08-15 18:09:45 -0300

    teste auth

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,5 +14,8 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         'stripe/*',
         '/api/posts',
+        '/api/login',
+        '/api/register',
+        '/api/logout',
     ];
 }
