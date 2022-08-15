dc93fbd52481eb0af55213af354509c9fb66fbd2
gustavobraga1001 <gubraro0@gmail.com>
2022-08-15 18:17:23 -0300

    teste auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,7 +17,7 @@ class AuthController extends Controller
         $request->validate([
             'name' => 'required|string',
             'email' => 'required|string|unique:users,email',
-            'password' => 'required|string|confirmed'
+            'password' => 'required|string'
         ]);
 
         $user = User::create([
